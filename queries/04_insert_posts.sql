2ad470ddeb1fdf789c316d6f9a1d3b1fedcea5d1
AJ Shannon <anjoshannon@gmail.com>
2018-06-14 15:12:58 -0400

    finished

---
+++ queries/04_insert_posts.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE posts (
+--     id serial  PRIMARY KEY,
+--     title varchar(50),
+--     body varchar(500),
+--     user_id INTEGER,
+-- )
+
+INSERT INTO posts
+(title, body, user_id)
+VALUES ('Title announcement', 'The API is awesome!', 1 );
+INSERT INTO posts
+(title, body, user_id)
+VALUES ('Just a test', 'This is just a test. I repeat, this is just a test.', 1 )
